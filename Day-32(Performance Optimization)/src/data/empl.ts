d280428d8f09285d3729b5b2d786d689892d8215
Roshni18opash <rpr.opash@gmail.com>
2026-02-03 17:46:14 +0530

    useMemo hook

---
+++ Day-32(Performance Optimization)/src/data/empl.ts
@@ -0,0 +1,19 @@
+export interface Employee{
+    id:number;
+    name:string;
+    department:string;
+    salary:number;
+}
+export const employees:Employee[]=[
+  { id: 1, name: "Amit", department: "IT", salary: 60000 },
+  { id: 2, name: "Riya", department: "HR", salary: 45000 },
+  { id: 3, name: "Rahul", department: "IT", salary: 70000 },
+  { id: 4, name: "Neha", department: "Finance", salary: 65000 },
+  { id: 5, name: "Karan", department: "HR", salary: 40000 },
+  { id: 6, name: "Sanya", department: "IT", salary: 55000 },
+  { id: 7, name: "Vikram", department: "Finance", salary: 68000 },
+  { id: 8, name: "Tanya", department: "HR", salary: 75000 },
+  { id: 9, name: "Aditya", department: "IT", salary: 72000 },
+  { id: 10, name: "Priya", department: "Finance", salary: 62000 }
+
+];
