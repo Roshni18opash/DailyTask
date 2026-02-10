cbd8e3bc724a360753a5cc7632493793ab2b1f96
Roshni18opash <rpr.opash@gmail.com>
2026-02-10 16:21:46 +0530

    create MVC

--- Day-42(Project_Structure)/src/controllers/user.controller.ts
+++ Day-42(Project_Structure)/src/controllers/user.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response }from "express";
-import { getAllTask } from "../services/task.service";
+import { getAllTask } from "../services/user.service";
 
 export const getTasks = (req: Request, res: Response): void => {
   const tasks = getAllTask();
--- Day-42(Project_Structure)/src/services/user.service.ts
+++ Day-42(Project_Structure)/src/services/user.service.ts
@@ -1,4 +1,4 @@
-import { task } from "../models/task.model";
+import { task } from "../models/user.model";
 import { Task } from "../types/task.type";
 
 export const getAllTask=():Task[]=>{
