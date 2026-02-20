f4cb8663c7aa1f183b1efc65ad60a64fe08c68dc
Roshni18opash <rpr.opash@gmail.com>
2026-02-20 10:38:46 +0530

    correction in delete function

--- Day-50(Backend_Mini_Project)/middleware/validateTokenHandler.js
+++ Day-50(Backend_Mini_Project)/middleware/validateTokenHandler.js
@@ -1,6 +1,5 @@
 const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
-
 const validateToken = asyncHandler(async (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
